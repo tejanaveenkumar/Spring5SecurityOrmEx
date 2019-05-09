fe78304b5aa7647824706516a2853b5830f7104b
TEJA <TEJA@TEJA-PC>
2019-05-09 14:44:39 +0530

    this is first commit

---
+++ src/main/java/com/app/dao/impl/UserDaoImpl.java
@@ -0,0 +1,48 @@
+package com.app.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.app.dao.IUserDao;
+import com.app.model.User;
+
+@Repository
+public class UserDaoImpl implements IUserDao{
+	@Autowired
+	private HibernateTemplate ht;
+	
+	public Integer saveUser(User user) {
+		return (Integer)ht.save(user);
+	}
+
+	@Override
+	public User findUserByEmail(String email) {
+       
+			User user=null; 
+			
+			//select * from usertab where email=?
+			@SuppressWarnings("unchecked")
+			List<User> userList=
+					(List<User>) ht.findByCriteria(
+							DetachedCriteria.forClass(User.class)
+							.add(Restrictions.eq("userEmail", email))
+							);
+			if(userList!=null && !userList.isEmpty()) {
+				user=userList.get(0);
+			}
+			
+			return user;
+		}
+	}
+	
+	
+	
+	
+	
+	
+
